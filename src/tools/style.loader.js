a2b5fe04524b43df155ddc4feabec78365d8d3b5
unknown <roberto.ianes.78@gmail.com>
2020-04-02 09:46:30 +0200

    update

--- src/tools/style.loader.js
+++ src/tools/style.loader.js
@@ -1,9 +1,9 @@
 import { css } from 'lit-element';
 const styleRules = Object.values(document.styleSheets).reduce((obj,styleSheet)=>{
-   return obj = {...obj,...styleSheet.cssRules}
-   },{})
+   return obj = [...obj,...Object.values(styleSheet.cssRules)]
+   },[])
    
-   let externalStyles =  Object.values(styleRules).map(rule=>rule.cssText).join('\n');
+   let externalStyles =  styleRules.map(rule=>rule.cssText).join('\n');
    externalStyles = externalStyles.replace(/html {/g,":host {");
    externalStyles = externalStyles.replace(/@import/g,"/@import");
    externalStyles = css([externalStyles]);
